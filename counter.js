7ef971ccf5736d5998dda481b4beddc8dd27fa57
Danil <das89@tpu.ru>
2023-11-23 17:47:57 +0700

    updated tasks page

--- counter.js
+++ counter.js
@@ -1,14 +1,18 @@
 let counter = 0;
-function count() {
-    counter++;
-    document.querySelector('h1').innerHTML = counter;
 
-    if (counter % 10 == 0) {
-        alert(`Count is now ${counter}`);
-        counter = 0;
-    }
+if (!localStorage.getItem("counter")) {
+    localStorage.setItem("counter", 0);
+}
+
+function count() {
+    let counter = localStorage.getItem("counter");
+    counter++;
+    document.querySelector("h1").innerHTML = counter;
+    localStorage.setItem("counter", counter);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("button").addEventListener("click", count);
+
+    setInterval(count, 1000);
 });
